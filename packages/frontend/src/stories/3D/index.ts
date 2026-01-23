bd570034c6387609efb2e807e179dce7c2936256
mujeeb-07 <mujeeb@mono.co>
2026-01-23 09:35:29 +0100

    Create Storybook for 3D components (Feature #557)
    - Created src/stories/3D/ directory for R3F component stories
    - Created decorators.tsx with:
      - CanvasWrapper for R3F Canvas setup
      - withCanvas() decorator factory
      - Pre-configured decorators (arcadeCabinet, componentPreview, lightingDemo)
      - Demo components (DemoBox, DemoGround, DemoSphere, DemoScene)
    - Created Scene.stories.tsx with stories for:
      - Default scene setup
      - Multiple objects
      - Shadow configurations
      - Camera variations (wide angle, telephoto)
      - Quality controls (SceneWithQuality)
      - FPS monitoring
    - Created LightingRig.stories.tsx with:
      - All preset variations (arcade, neon, dramatic, soft, game-over, victory)
      - Configuration stories (intensity, helpers, shadow quality)
      - Custom point lights example
      - Specialized variants (ArcadeLighting, NeonLighting, GameStateLighting)
      - All presets gallery comparison
    - Fixed lint error in visual-regression-utils.ts (const for unused variable)

---
+++ packages/frontend/src/stories/3D/index.ts
@@ -0,0 +1,23 @@
+/**
+ * 3D Component Stories
+ *
+ * This directory contains Storybook stories for all React Three Fiber
+ * components used in the x402 Arcade project.
+ *
+ * @module stories/3D
+ */
+
+// Re-export decorators and helpers for use in other stories
+export {
+  CanvasWrapper,
+  withCanvas,
+  defaultCanvasDecorator,
+  arcadeCabinetDecorator,
+  componentPreviewDecorator,
+  lightingDemoDecorator,
+  DemoBox,
+  DemoGround,
+  DemoSphere,
+  DemoScene,
+  type CanvasDecoratorOptions,
+} from './decorators'
